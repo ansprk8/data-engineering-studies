f3f2ba3b91d786e144b08d6298adfef42b1c18cf
ansprk8 <ansoprikk@gmail.com>
2025-12-01 03:24:51 -0300

    basic_exercises.py

--- logic-basics/basic_exercises.py
+++ logic-basics/basic_exercises.py
@@ -23,6 +23,9 @@ nome = input('Qual seu nome?: ')
 idade = int(input('Qual sua idade?: '))
 altura = float(input('Qual a sua altura em metros?: '))
 print(f'Olá, {nome}, você tem {idade} anos e mede {altura} metros!')
+
+
+
 # ==================================================
 # Calculadora com Operadores
 
@@ -97,4 +100,70 @@ print(f'A média entre as notas de João, Maria e Pedro é igual a {(a+b+c)/3}')
 
 media_pon = (5*1 + 12*2 + 20*3 + 15*4) / (1+2+3+4)
 print(f'A média ponderada dos números 5, 12, 20 e 15 com pesos respectivamente iguais a 1, 2, 3 e 4 é equivalente a: {media_pon}')
+
+
+
+# ==================================================
+# Editando Textos
+# Editing Texts
+# ==================================================
+# Q1 (PT): Crie uma variável com uma frase e imprima-a.
+# Q1 (EN): Create a variable with a sentence and print it.
+
+frase = 'Olá Mundo!'
+print(frase)
+# ---
+# Q2 (PT): Solicite uma frase e imprima-a.
+# Q2 (EN): Ask for a sentence and print it.
+
+frase = input('Digite uma frase: ')
+print(frase)
+# ---
+# Q3 (PT): Solicite uma frase e imprima em maiúsculas.
+# Q3 (EN): Ask for a sentence and print it in uppercase.
+
+frase = input('Digite uma frase: ')
+print(frase.upper())
+# ---
+# Q4 (PT): Solicite uma frase e imprima em minúsculas.
+# Q4 (EN): Ask for a sentence and print it in lowercase.
+
+frase = input('Digite uma frase: ')
+print(frase.lower())
+# ---
+# Q5 (PT): Crie uma variável com uma frase e imprima-a sem espaços no início e fim.
+# Q5 (EN): Create a variable with a sentence and print it without leading or trailing spaces.
+
+frase = ' Olá mundo  '
+print(frase.strip())
+# ---
+# Q6 (PT): Solicite uma frase e imprima-a sem espaços no início e fim.
+# Q6 (EN): Ask for a sentence and print it without leading or trailing spaces.
+
+frase = input('Digite uma frase: ')
+print(frase.strip())
+# ---
+# Q7 (PT): Solicite uma frase e imprima-a sem espaços no início e fim e em maiúsculas.
+# Q7 (EN): Ask for a sentence and print it stripped and in uppercase.
+
+frase = input('Digite uma frase: ')
+print(frase.strip().upper())
+# ---
+# Q8 (PT): Solicite uma frase e substitua todas as letras "e" por "f".
+# Q8 (EN): Ask for a sentence and replace all "e" letters with "f".
+
+frase = input('Digite uma frase: ')
+print(frase.replace('e', 'f'))
+# ---
+# Q9 (PT): Solicite uma frase e substitua todas as letras "a" pelo caractere "@".
+# Q9 (EN): Ask for a sentence and replace all "a" letters with "@".
+
+frase = input('Digite uma frase: ')
+print(frase.replace('a', '@'))
+# ---
+# Q10 (PT): Solicite uma frase e substitua todas as consoantes "s" pelo caractere "$".
+# Q10 (EN): Ask for a sentence and replace all consonants "s" with "$".
+
+frase = input('Digite uma frase: ')
+print(frase.replace('s', '$'))
 # ---
